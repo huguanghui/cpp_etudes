726b9bbfd8fe897338813e58ab6e40920268f155
huguanghui <522146829@qq.com>
2021-01-04 07:43:00 +0800

    feat(perl): 更新perl的练习脚本

--- perl_test/test01.pl
+++ perl_test/test01.pl
@@ -13,7 +13,7 @@ sub ensure_ag_installed {
   my ($ag_path) = map {chomp;
     $_} qx(which ag 2>/dev/null);
   if (!defined($ag_path) || (!-e $ag_path)) {
-    printf STDERR "ag is missing, please install ag at first, refer to https://github.com/ggreer/the_silver_searcher\n"
+    printf STDERR "ag is missing, please install ag at first, refer to https://github.com/ggreer/the_silver_searcher\n";
     exit 1; 
   }
   
@@ -21,7 +21,7 @@ sub ensure_ag_installed {
 
 ensure_ag_installed;
 
-my $ignore_pattern = join "", map {" --ignore '$_' "} qw(*test* *benchmark* "CMakeFiles* *contrib/* *thirdparty/* *3rd-[pP]arty/* *3rd[pP]arty/*")
+my $ignore_pattern = join "", map {" --ignore '$_' "} qw(*test* *benchmark* "CMakeFiles* *contrib/* *thirdparty/* *3rd-[pP]arty/* *3rd[pP]arty/*");
 my $cpp_filename_pattern = qq/'\\.(c|cc|cpp|C|h|hh|hpp|H)\$'/;
 
 sub multiline_break_enabled() {
@@ -30,3 +30,5 @@ sub multiline_break_enabled() {
   return defined($enabled) && $enabled eq "enabled";
 }
 
+print "$ignore_pattern\n";
+print "$cpp_filename_pattern\n";
